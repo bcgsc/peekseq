40875fdafbe01a5c9ae994b77958e9e59546c40b
René <rwarren@bcgsc.ca>
2023-06-16 09:26:48 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,153 @@
+![Logo](https://github.com/bcgsc/cpkseq/blob/main/cpkseq-logo.png)
+
 # cpkseq
-De novo [protein] coding potential calculator using a k-mer approach
+## De novo [protein] coding potential calculator using a k-mer approach
+### 2023
+
+
+## Contents
+
+1. [Description](#description)
+2. [Implementation and requirements](#implementation)
+3. [Install](#install)
+4. [Dependencies](#dep)
+5. [Documentation](#docs)
+6. [Citing cpkseq](#cite)
+7. [Credits](#credits)
+8. [Running cpkseq](#run)
+9. [Test data](#data)
+10. [Output](#output)
+11. [Algorithm](#algorithm)
+12. [Quick reference](#quickref)
+13. [Generating plots](#bplot)
+14. [License](#license)
+
+
+## Description <a name=description></a>
+
+cpkseq systematically processes the k-mers of a supplied DNA sequence and computes regions with coding potential, using target codon translation tables
+
+cpkseq is used to quickly survey regions with [protein] coding potential on all 6 frames
+
+
+## Implementation and requirements <a name=implementation></a>
+
+cpkseq is developed in PERL and runs on any system where PERL is installed.
+
+
+## Install <a name=install></a>
+
+Clone and enter the unikseq directory.
+<pre>
+git clone https://github.com/bcgsc/cpkseq
+cd cpkseq
+</pre>
+
+
+
+## Dependencies <a name=dep></a>
+
+If PERL is installed on your system, you're good to go (no additional libraries needed, nor dependencies).
+
+
+## Documentation <a name=docs></a>
+
+Refer to the README.md file on how to install and run cpkseq. Read the 
+
+
+## Citing cpkseq <a name=cite></a>
+
+Thank you for your [![Stars](https://img.shields.io/github/stars/bcgsc/cpkseq.svg)](https://github.com/bcgsc/cpkseq/stargazers) and for using, developing and promoting this free software!
+
+If you use unikseq in your research, please cite: 
+
+TBD
+
+## Credits <a name=credits></a>
+<pre>
+cpkseq concept, algorithm design, implementation: Rene L Warren
+</pre>
+
+
+## Running cpkseq <a name=run></a>
+
+<pre>
+Usage: ./cpkseq.pl VERSION
+ -f FASTA (DNA/RNA) file (required)
+ -k length (option, default: -k 90)
+ -c genetic code translation table id (option, default: -c 1 [standard])
+ -s min. reference FASTA region [size] (bp) to output (option, default: -s 270 bp)
+ -v output tsv file (option, -v 1==yes -v 0==no [default])
+</pre>
+
+Notes:
+<pre>
+
+
+ -f FASTA (DNA/RNA) file (required)
+
+ -k length (option, default: -k 90)
+
+ -c genetic code translation table id (option, default: -c 1 [standard])
+
+ -s min. reference FASTA region [size] (bp) to output (option, default: -s 270 bp)
+
+ -v output tsv file (option, -v 1==yes -v 0==no [default])
+
+
+</pre>
+
+### Test data <a name=data></a>
+---------
+
+TBD
+
+## Output  <a name=output></a>
+
+1) TSV file 
+
+
+2) FASTA file (*DNA.fa)
+
+   A multi-FASTA 
+
+3) FASTA file (*PROTEIN.fa)
+
+
+4) LOG file (.log)
+
+   Captures the verbose STDOUT in a log file, showing the progress of the program.    
+
+
+## Algorithm design and implementation <a name=algorithm></a>
+
+TBD
+ 
+## Quick reference <a name=quickref></a>
+
+TBD
+
+## Generating plots <a name=bplot></a>
+
+TBD
+
+## License <a name=license></a>
+
+Unikseq Copyright (c) 2020-2023 British Columbia Cancer Agency Branch.  All rights reserved.
+
+Unikseq is released under the GNU General Public License v3
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, version 3.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+For commercial licensing options, please contact
+Patrick Rebstein <prebstein@bccancer.bc.ca>
---
+++ cpkseq.pl
@@ -0,0 +1,440 @@
+#!/usr/bin/env perl
+
+#AUTHOR
+#   Rene Warren
+#   rwarren at bcgsc.ca
+
+#NAME
+# cpkseq
+
+#SYNOPSIS
+# Identifies DNA regions with coding potential de novo, using a k-mer approach
+
+#DOCUMENTATION
+#   README.md distributed with this software
+#   We hope this code is useful to you -- Please send comments & suggestions to rwarren * bcgsc.ca
+
+#LICENSE
+#cpkseq Copyright (c) 2023 British Columbia Cancer Agency Branch.  All rights reserved.
+#cpkseq is released under the GNU General Public License v3
+
+use strict;
+use Getopt::Std;
+
+use vars qw($opt_k $opt_f $opt_s $opt_v $opt_c);
+getopts('k:f:s:v:c:');
+
+my $version = "v0.0.1";
+my ($k,$tsvflag,$code) = (90,0,1);
+my $regsz = 3 * $k;
+system("clear");
+
+if(! $opt_f){
+   print "Usage: $0 $version\n";
+   print " -f FASTA (DNA/RNA) file (required)\n";
+   print " -k length (option, default: -k $k)\n";
+   print " -c genetic code translation table id (option, default: -c $code [standard])\n";
+   print " -s min. reference FASTA region [size] (bp) to output (option, default: -s $regsz bp)\n";
+   die " -v output tsv file (option, -v 1==yes -v 0==no [default])\n";
+}
+
+### Fetch options
+
+my $f = $opt_f; #reference
+$k = $opt_k if($opt_k);
+$regsz = $opt_s if($opt_s);
+$tsvflag = $opt_v if($opt_v);
+$code = $opt_c if($opt_c);
+
+
+###Prepare output
+#-----
+my $fn = "cpkseq_" . $version . "-f_" . $f . "-k" . $k;
+my $tsv= $fn . "-frameKmers.tsv";
+
+$fn .= "-s" . $regsz . "-c" . $code;
+
+my $dna=$fn . "-codingDNA.fa";
+my $pep=$fn . "-codingPROTEIN.fa";
+
+my $log=$fn . ".log";
+
+open(LOG,">$log") || die "Can't write to $log -- fatal.\n";
+
+my $message = "\nRunning: $0 $version\n\t-k $k\n\t-f $f\n\t-s $regsz\n\t-c $code\n\t-v $tsvflag\n";
+
+print $message;
+print LOG $message;
+
+
+###Checking files and options
+#-----
+if(! -e $f){
+   die "Invalid file: $f -- fatal\n";
+}
+
+if($k % 3 != 0){
+   die "-k $k must be a multiple of 3 -- fatal.\n";
+}
+
+&evalCP($f,$k,$dna,$pep,$regsz,$tsv,$code,$tsvflag);
+
+$message = "done.\n";
+$message .= "-" x 30, "\n";
+$message .= "\nOutput in-frame regions >= $regsz bp in:\n$dna\n$pep\n";
+$message .= "\nOutput $k-mers frames:\n$tsv\ndone.\n" if($tsvflag);
+print $message;
+print LOG $message;
+
+close LOG;
+
+exit;
+
+#--------------------------------
+sub evalCP{
+  
+   my ($f,$k,$dna,$pep,$regsz,$tsv,$code,$tsvflag) = @_;
+   
+   my ($head,$prevhead,$seq,$preventry) = ("","","","");
+
+   open(DNA,">$dna") || die "Can't write $dna -- fatal.\n";
+   open(PEP,">$pep") || die "Can't write $pep -- fatal.\n";
+
+   if($tsvflag){
+      open(TSV,">$tsv") || die "Can't write $tsv -- fatal.\n";
+      print TSV "header\tposition\tframe\tcoding\n";
+   }
+
+   my ($codon2aa,$startcodon) = &initializeAA($code);
+
+   open(IN,$f) || die "Can't read $f -- fatal.\n";
+   while(<IN>){
+      s/\r\n/\n/g;### DOS to UNIX
+      chomp;
+
+      if(/^\>(\S+)/){
+         my $entry = $1;
+         $head = $entry;#$_;
+         if($prevhead ne $head && $prevhead ne "" && $seq ne ""){
+            &printOutput($codon2aa,$startcodon,$prevhead,$seq,$preventry,$k,$regsz,$tsvflag);
+         }
+         $seq = "";
+         $prevhead = $head;
+         $preventry = $entry;
+      }else{
+         my $seqstretch = $1 if(/^(\S+)/); ###this prevents DOS new lines from messing up the TSV output
+         $seq .= uc($seqstretch);
+      }
+   }
+  
+   &printOutput($codon2aa,$startcodon,$prevhead,$seq,$preventry,$k,$regsz,$tsvflag);
+
+   close DNA;
+   close PEP;
+   close TSV if($tsvflag);   
+}
+
+#--------------------------------
+sub printOutput{
+
+   my ($codon2aa,$startcodon,$head,$seq,$preventry,$k,$regsz,$tsvflag) = @_;
+
+   my ($initial,$unique,$notunique,$sum,$sumout,$buffer) = (-1,0,0,0,0,0);
+   my $s;
+
+   for(my $pos=0;$pos<=(length($seq)-$k);$pos++){
+      my $kmer = substr($seq,$pos,$k);
+      my $codon = substr($kmer,($k-3),3);
+
+      $kmer =~ tr/U/T/; ### handles RNA U>>>T
+      my $rckmer = &reverseComplement($kmer);
+
+      my $frame = ($pos % 3) + 1;
+      my $framerev = -1 * $frame;
+
+      my $aamer = &dna2protein($kmer,$codon2aa);
+      my $rcaamer = &dna2protein($rckmer,$codon2aa);
+
+      my $bit=1;
+      if($aamer=~/\*/ || $aamer=~/X/){
+         $bit=0;
+         my $tmplen = length($s->{$frame}{'seq'});
+         if($tmplen >= $regsz){
+            $s->{$frame}{'seq'} .= $codon;
+            $s->{$frame}{'end'} = $pos + $k;
+            ###
+            my $lcdna = lc($s->{$frame}{'seq'});
+            my @ntarr = ( $lcdna =~ m/.../g );
+            my $flag=0;
+            my $newnt="";
+            my $newaa="";
+            foreach my $cd (@ntarr){
+               my $aa = lc(&dna2protein($cd,$codon2aa));
+               if(defined $startcodon->{$cd}){$flag=1;}
+               if($flag){
+                  $newnt .= uc($cd);
+                  $newaa .= uc($aa);
+               }else{
+                  $newnt .= $cd;
+                  $newaa .= $aa;
+               }
+            }
+            ###
+            print DNA ">$head $s->{$frame}{'start'}-$s->{$frame}{'end'} length:$tmplen frame:$frame\n$newnt\n";
+            my $aalen = length($newaa);
+            print PEP ">$head $s->{$frame}{'start'}-$s->{$frame}{'end'} length:$aalen frame:$frame\n$newaa\n";
+         }
+         $s->{$frame}{'start'} = 0;
+         $s->{$frame}{'seq'} = ""; 
+      }else{
+         if($s->{$frame}{'seq'} eq ""){
+            $s->{$frame}{'seq'} = $kmer;
+            $s->{$frame}{'start'} = $pos;
+         }else{
+            $s->{$frame}{'seq'} .= $codon;
+            $s->{$frame}{'end'} = $pos + $k;
+         }##
+      }
+      my $rcbit=-1;
+      if($rcaamer=~/\*/ || $rcaamer=~/X/){
+         $rcbit=0;
+         my $tmplen = length($s->{$framerev}{'seq'});
+         if($tmplen >= $regsz){
+            my $rc = reverseComplement($s->{$framerev}{'seq'});
+            my $lcdna = lc($rc);
+            my @ntarr = ( $lcdna =~ m/.../g );
+            my $flag=0;
+            my $newnt="";
+            my $newaa="";
+            foreach my $cd (@ntarr){
+               my $aa = lc(&dna2protein($cd,$codon2aa));
+               if(defined $startcodon->{$cd}){$flag=1;}
+               if($flag){
+                  $newnt .= uc($cd);
+                  $newaa .= uc($aa);
+               }else{
+                  $newnt .= $cd;
+                  $newaa .= $aa;
+               }
+            }
+            print DNA ">$head $s->{$framerev}{'end'}-$s->{$framerev}{'start'} length:$tmplen frame:$framerev\n$newnt\n";
+            my $aalen = length($newaa);
+            print PEP ">$head $s->{$framerev}{'end'}-$s->{$framerev}{'start'} length:$aalen frame:$framerev\n$newaa\n";
+         }
+         $s->{$framerev}{'start'} = $pos;
+         $s->{$framerev}{'seq'} = $kmer;
+      }else{
+         if($s->{$framerev}{'seq'} eq ""){
+            $s->{$framerev}{'seq'} = $kmer;
+            $s->{$framerev}{'start'} = $pos;
+         }else{
+            $s->{$framerev}{'seq'} .= $codon;
+            $s->{$framerev}{'end'} = $pos + $k;
+         }
+      }
+
+      print TSV "$head\t$pos\t$frame\t$bit\n$head\t$pos\t$framerev\t$rcbit\n" if($tsvflag);
+
+   }
+}
+
+#--------------------------------
+sub initializeAA{
+
+    my $code=shift;
+
+    my $codon2aa;
+    my $startcodon;
+
+    $codon2aa->{"ttt"}= "F"; $codon2aa->{"ttc"}= "F"; $codon2aa->{"tta"}="L";
+    $codon2aa->{"ttg"}= "L"; $codon2aa->{"tct"}= "S"; $codon2aa->{"tcc"}= "S";
+    $codon2aa->{"tca"}= "S"; $codon2aa->{"tcg"}= "S"; $codon2aa->{"tat"}= "Y";
+    $codon2aa->{"tac"}= "Y"; $codon2aa->{"tgt"}= "C"; $codon2aa->{"tgc"}= "C";
+    $codon2aa->{"ctt"}= "L"; $codon2aa->{"ctc"}= "L"; $codon2aa->{"cta"}= "L";
+    $codon2aa->{"ctg"}= "L"; $codon2aa->{"cct"}= "P"; $codon2aa->{"ccc"}= "P";
+    $codon2aa->{"cca"}= "P"; $codon2aa->{"ccg"}= "P"; $codon2aa->{"cat"}= "H";
+    $codon2aa->{"cac"}= "H"; $codon2aa->{"caa"}= "Q"; $codon2aa->{"cag"}= "Q";
+    $codon2aa->{"cgt"}= "R"; $codon2aa->{"cgc"}= "R"; $codon2aa->{"cga"}= "R";
+    $codon2aa->{"cgg"}= "R"; $codon2aa->{"att"}= "I"; $codon2aa->{"atc"}= "I";
+    $codon2aa->{"ata"}= "I"; $codon2aa->{"atg"}= "M"; $codon2aa->{"act"}= "T";
+    $codon2aa->{"acc"}= "T"; $codon2aa->{"aca"}= "T"; $codon2aa->{"acg"}= "T";
+    $codon2aa->{"aat"}= "N"; $codon2aa->{"aac"}= "N"; $codon2aa->{"aaa"}= "K";
+    $codon2aa->{"aag"}= "K"; $codon2aa->{"agt"}= "S"; $codon2aa->{"agc"}= "S";
+    $codon2aa->{"aga"}= "R"; $codon2aa->{"agg"}= "R"; $codon2aa->{"gtt"}= "V";
+    $codon2aa->{"gtc"}= "V"; $codon2aa->{"gta"}= "V"; $codon2aa->{"gtg"}= "V";
+    $codon2aa->{"gct"}= "A"; $codon2aa->{"gcc"}= "A"; $codon2aa->{"gca"}= "A";
+    $codon2aa->{"gcg"}= "A"; $codon2aa->{"gat"}= "D"; $codon2aa->{"gac"}= "D";
+    $codon2aa->{"gaa"}= "E"; $codon2aa->{"gag"}= "E"; $codon2aa->{"ggt"}= "G";
+    $codon2aa->{"ggc"}= "G"; $codon2aa->{"gga"}= "G"; $codon2aa->{"ggg"}= "G";
+    $codon2aa->{"tgg"}= "W";
+    $codon2aa->{"tag"}= "*";
+    $codon2aa->{"tga"}= "*";
+    $codon2aa->{"taa"}= "*";
+
+    $startcodon->{"atg"} = "M";
+    #The standard code currently allows initiation from UUG and CUG in addition to AUG.
+    $startcodon->{"ttg"} = "L";
+    $startcodon->{"ctg"} = "L";
+    $startcodon->{"gtg"} = "V";
+
+    #There are Differences from the Standard Code
+    #https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?mode=c#top
+    #1. The Standard Code
+    #2. The Vertebrate Mitochondrial Code
+    #3. The Yeast Mitochondrial Code
+    #4. The Mold, Protozoan, and Coelenterate Mitochondrial Code and the Mycoplasma/Spiroplasma Code
+    #5. The Invertebrate Mitochondrial Code
+    #6. The Ciliate, Dasycladacean and Hexamita Nuclear Code
+    #9. The Echinoderm and Flatworm Mitochondrial Code
+    #10. The Euplotid Nuclear Code
+    #11. The Bacterial, Archaeal and Plant Plastid Code
+    #12. The Alternative Yeast Nuclear Code
+    #13. The Ascidian Mitochondrial Code
+    #14. The Alternative Flatworm Mitochondrial Code
+    #16. Chlorophycean Mitochondrial Code
+    #21. Trematode Mitochondrial Code
+    #22. Scenedesmus obliquus Mitochondrial Code
+    #23. Thraustochytrium Mitochondrial Code
+    #24. Rhabdopleuridae Mitochondrial Code
+    #25. Candidate Division SR1 and Gracilibacteria Code
+    #26. Pachysolen tannophilus Nuclear Code
+    #27. Karyorelict Nuclear Code
+    #28. Condylostoma Nuclear Code
+    #29. Mesodinium Nuclear Code
+    #30. Peritrich Nuclear Code
+    #31. Blastocrithidia Nuclear Code
+    #33. Cephalodiscidae Mitochondrial UAA-Tyr Code
+
+    if($code==2){
+       $codon2aa->{"aga"}="*";
+       $codon2aa->{"agg"}="*";
+       $codon2aa->{"ata"}="M";
+       $codon2aa->{"tga"}="W";
+       $startcodon->{"ata"}="M";
+       $startcodon->{"att"}="I";
+       $startcodon->{"atc"}="I";
+    }elsif($code==3){
+       $codon2aa->{"ata"}="M"; 
+       $codon2aa->{"ctt"}="T";
+       $codon2aa->{"ctc"}="T";
+       $codon2aa->{"cta"}="T";
+       $codon2aa->{"ctg"}="T";
+       $codon2aa->{"tga"}="W";
+       $startcodon->{"gtg"}="V";
+    }elsif($code==4){
+       $codon2aa->{"tga"}="W";
+    }elsif($code==5){
+       $codon2aa->{"tga"}="W";
+       $codon2aa->{"ata"}="M";
+       $codon2aa->{"agg"}="S";
+       $codon2aa->{"aga"}="S";
+       $startcodon->{"ata"}="M";
+       $startcodon->{"att"}="I";
+    }elsif($code==6){
+       $codon2aa->{"taa"}="Q";
+       $codon2aa->{"tag"}="Q";
+    }elsif($code==9){
+       $codon2aa->{"aaa"}="N";
+       $codon2aa->{"aga"}="S";
+       $codon2aa->{"agg"}="S";
+       $codon2aa->{"tga"}="W";
+       $startcodon->{"gtg"}="V";
+       $startcodon->{"ttg"}="L";
+    }elsif($code==10){
+       $codon2aa->{"tga"}="C";
+    }elsif($code==12){
+       $codon2aa->{"ctg"}="S";
+    }elsif($code==13){
+       $codon2aa->{"aga"}="G";
+       $codon2aa->{"agg"}="G";
+       $codon2aa->{"ata"}="M";
+       $codon2aa->{"tga"}="W";
+       $startcodon->{"gtg"}="V";
+       $startcodon->{"ttg"}="L";
+       $startcodon->{"ata"}="M";
+    }elsif($code==14){
+       $codon2aa->{"aaa"}="N";
+       $codon2aa->{"aga"}="S";
+       $codon2aa->{"agg"}="S";
+       $codon2aa->{"taa"}="Y";
+       $codon2aa->{"tga"}="W";
+    }elsif($code==16){
+       $codon2aa->{"tag"}="L";
+    }elsif($code==21){
+       $codon2aa->{"tga"}="W";
+       $codon2aa->{"ata"}="M";
+       $codon2aa->{"aga"}="S";
+       $codon2aa->{"agg"}="S";
+       $codon2aa->{"aaa"}="N";
+    }elsif($code==22){
+       $codon2aa->{"tca"}="*";
+       $codon2aa->{"tag"}="L";
+    }elsif($code==24){
+       $codon2aa->{"aga"}="S";
+       $codon2aa->{"agg"}="K";
+       $codon2aa->{"tga"}="W";
+    }elsif($code==25){
+       $codon2aa->{"tga"}="G";
+       $startcodon->{"gtg"}="V";
+       $startcodon->{"ttg"}="L";
+    }elsif($code==26){
+       $codon2aa->{"ctg"}="A";
+       $startcodon->{"gtg"}="V";
+       $startcodon->{"ttg"}="L";
+    }elsif($code==27){
+       $codon2aa->{"tag"}="Q";
+       $codon2aa->{"taa"}="Q";
+       #or $codon2aa->{"tga"}="W";
+    }elsif($code==28){
+       #or$codon2aa->{"tag"}="Q";
+       #or $codon2aa->{"taa"}="Q";
+       #or $codon2aa->{"tga"}="W";
+    }elsif($code==29){
+       $codon2aa->{"tag"}="Y";
+       $codon2aa->{"taa"}="Y";
+    }elsif($code==30){
+       $codon2aa->{"tag"}="E";
+       $codon2aa->{"taa"}="E";
+    }elsif($code==31){
+       $codon2aa->{"tga"}="W";
+       #or $codon2aa->{"tag"}="E";
+       #or $codon2aa->{"taa"}="E";
+    }elsif($code==33){
+       $codon2aa->{"aga"}="S";
+       $codon2aa->{"taa"}="Y";
+       $codon2aa->{"tga"}="W";
+       $codon2aa->{"agg"}="K";
+    }
+
+    return $codon2aa,$startcodon;
+}
+
+#--------------------------------
+sub dna2protein{
+
+    my ($seq,$codon2aa) = @_;
+
+    my $seq = lc($seq);
+
+    my @codonlist = ( $seq =~ m/.../g );
+
+    my $aa_seq = "";
+
+    foreach my $codon (@codonlist){
+       #print ">>>$codon<<<\n";
+       if (defined $codon2aa->{$codon}){
+          $aa_seq.=$codon2aa->{$codon};
+       }else{
+          $aa_seq.="X";
+       }
+    }
+    return $aa_seq;
+}
+
+#--------------------------------
+sub reverseComplement{
+   $_ = shift;
+   $_ = uc();
+   tr/ATGCYRKMBDHV/TACGRYMKVHDB/;
+   return (reverse());
+}
